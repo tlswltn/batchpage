fa630dd35bf8fdd02dc2563c40bd9e12c3e50853
tlswltn <tlswltn0124@nate.com>
2021-03-25 15:29:21 +0900

    Updates

---
+++ precache-manifest.42d320db3deae42c0a1c8bcfcfe0a18b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5031e304c42da941cfa6",
+    "url": "/batchpage/static/css/main.6957eadb.chunk.css"
+  },
+  {
+    "revision": "5031e304c42da941cfa6",
+    "url": "/batchpage/static/js/main.b29cbfc6.chunk.js"
+  },
+  {
+    "revision": "c78ff9fa0f6c2243ae65",
+    "url": "/batchpage/static/js/runtime~main.dda9c4a8.js"
+  },
+  {
+    "revision": "c6d06398a03c7efd8d38",
+    "url": "/batchpage/static/js/2.ec6b348a.chunk.js"
+  },
+  {
+    "revision": "ee7d5376619a71c33056",
+    "url": "/batchpage/static/js/3.fb789e31.chunk.js"
+  },
+  {
+    "revision": "229d1821d6843822bd1bfdf3a6a3b5c4",
+    "url": "/batchpage/index.html"
+  }
+];
