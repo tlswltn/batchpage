05c990bfdabf8c5c67a183c3913f33dbc4a0ed9f
tlswltn <tlswltn0124@nate.com>
2021-03-25 16:27:35 +0900

    Updates

---
+++ precache-manifest.6f9ed01b6baed851aea69bbb1a4f31d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd26273b0997d4723e0e",
+    "url": "/batchpage/static/css/main.0225b53a.chunk.css"
+  },
+  {
+    "revision": "dd26273b0997d4723e0e",
+    "url": "/batchpage/static/js/main.1b9620be.chunk.js"
+  },
+  {
+    "revision": "eee225b70e0ed25c2e25",
+    "url": "/batchpage/static/js/runtime~main.ce9d27be.js"
+  },
+  {
+    "revision": "5f8c190e0fe40a678879",
+    "url": "/batchpage/static/js/2.cf3d82a4.chunk.js"
+  },
+  {
+    "revision": "d78c1b9ea75ed9c3f7b8",
+    "url": "/batchpage/static/js/3.6eab057b.chunk.js"
+  },
+  {
+    "revision": "40bc970fcf7a3ecd877d56d89d84313e",
+    "url": "/batchpage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/batchpage/precache-manifest.42d320db3deae42c0a1c8bcfcfe0a18b.js"
+  "/batchpage/precache-manifest.6f9ed01b6baed851aea69bbb1a4f31d7.js"
 );
 
 workbox.clientsClaim();
